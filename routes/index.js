86f1ae5c6598d9047151dd057ae9bfbc82d3362c
Nguyen Nguyen <n.dinhnguyen95@gmail.com>
2020-04-16 10:21:29 +0700

    fixed loadData

--- routes/index.js
+++ routes/index.js
@@ -7,13 +7,14 @@ const path = require('path')
 
 const pathToUpload = path.join(__dirname, "../public/uploads/originals")
 
-const data = loadData();
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'MEMEMAKER' });
 });
 
 router.get('/browse', (req, res) => {
+  const data = loadData();
   return res.render('allImages', {images: data})
 })
 
@@ -25,17 +26,18 @@ router.post('/upload', upload, (req, res) => {
 
   console.log('index.js',`${pathToUpload}/${file.originalname}`)
 
-  Jimp.read(`${pathToUpload}/${file.originalname}`)
-  .then(item => {
-      return item
-      .resize(400, 300)
-      .quality(60)
-      .greyscale()
-      .write(file.originalname)
-  })
-  .catch(err => {
-      console.error(err)
-  })
+  // Jimp.read(`${pathToUpload}/${file.originalname}`)
+  // .then(item => {
+  //     return item
+  //     .resize(400, 300)
+  //     .quality(60)
+  //     .greyscale()
+  //     .write(file.originalname)
+  // })
+  // .catch(err => {
+  //     console.error(err)
+  // })
+  const data = loadData();
 
   console.log('data index.js',data)
 
